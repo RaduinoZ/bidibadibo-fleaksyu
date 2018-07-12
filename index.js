0152efc733606608546ac51475213a45f88f38ea
Raduino <mimi02rtg@gmail.com>
2018-07-12 19:56:26 +0300

    urban dictionary yay

--- index.js
+++ index.js
@@ -332,10 +332,32 @@ client.on("message", async message => {
             }
         });
     }
+
     if (command === 'cat') {
 		const { body } = await snekfetch.get('https://aws.random.cat/meow');
 
 		message.channel.send(body.file);
+	}
+
+  if (command === 'urban') {
+		const { body } = await snekfetch.get('https://api.urbandictionary.com/v0/define').query({ term: args.join(' ') });
+
+		if (body.result_type === 'no_results') {
+			return message.channel.send(`No results found for **${args.join(' ')}**`);
+		}
+
+		const [answer] = body.list;
+
+		const embed = new Discord.RichEmbed()
+			.setColor('#EFFF00')
+			.setTitle(answer.word)
+			.setURL(answer.permalink)
+			.addField('Definition', trim(answer.definition, 1024))
+			.addField('Example', trim(answer.example, 1024))
+			.addField('Rating', `${answer.thumbs_up} thumbs up.\n${answer.thumbs_down} thumbs down.`)
+			.setFooter(`Tags: ${body.tags.join(', ')}`);
+
+		message.channel.send(embed);
 	}
 });
 
